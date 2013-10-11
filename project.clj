4c9533a9b2742c0bf74766e05e688b414bf7e10a
Christopher Rosengren <christopher.rosengren@gmail.com>
2013-10-11 22:08:46 +1100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject fs "0.4.4"
+  :description "File System Utilities"
+  :dependencies [[extensions "0.1.5"]
+                 [clj-time "0.6.0"]
+                 [org.apache.commons/commons-vfs2 "2.0"]
+                 [org.clojure/clojure "1.5.1"]])
