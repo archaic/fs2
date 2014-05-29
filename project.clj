3238a7d63e990630e4327f868e3f8235ea079963
Christopher Rosengren <christopher.rosengren@gmail.com>
2014-05-29 14:12:30 +1000

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.eoneq/fs "0.4.9"
+(defproject com.eoneq/fs2 "0.4.10"
   :description "File System Utilities"
-  :dependencies [[extension "0.2.4"]
+  :dependencies [[extension "0.2.5"]
                  [clj-time "0.7.0"]
                  [org.apache.commons/commons-vfs2 "2.0"]
                  [org.clojure/clojure "1.6.0"]])
