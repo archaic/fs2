94c0ea3c6ea4f48ee2f8a7ac11d50e7af86e06e5
Christopher Rosengren <christopher.rosengren@gmail.com>
2014-06-02 12:43:47 +1000

    Initial commit

---
+++ src/fs2/core.clj
@@ -0,0 +1,118 @@
+(ns fs2.core
+  (:require [extension.zip :as azip :refer [postwalk!]]
+            [clojure.zip :as zip :refer [zipper]])
+  (:import [java.nio.file Files LinkOption Path Paths CopyOption StandardCopyOption]
+           [java.nio.file.attribute FileAttribute]))
+
+(defn ->path
+  [file]
+  (cond (string? file) (Paths/get file (into-array String []))
+        (instance? Path file) file))
+
+(defn file?
+  [path]
+  (Files/isRegularFile (->path path) (LinkOption/values)))
+
+(defn directory?
+  [path]
+  (Files/isDirectory (->path path) (LinkOption/values)))
+
+(defn children
+  [path]
+  (iterator-seq (.iterator (Files/newDirectoryStream path))))
+
+(defn path-zip
+  "Returns a (non editable) zipper for path elements, given a root element"
+  [root]
+  (zipper directory? children nil root))
+
+(defn directories
+  [path]
+  (filter directory? (children path)))
+
+(defn rename
+  "Renames all regular files in path from keys in m to values"
+  [path m]
+  (doseq [[source target] m]
+    (try (Files/move (.resolve path source)
+                     (.resolve path target) (into-array CopyOption []))
+         (catch Exception e (println (.getMessage e))))))
+
+(defn mkdir
+  ([path] (Files/createDirectory (->path path) (into-array FileAttribute [])))
+  ([path dir] (mkdir (.resolve (->path path) dir))))
+
+(defn rmdir
+  [path]
+  (let [path (->path path)]
+    (when (directory? path)
+      (postwalk! (path-zip path)
+                 (fn [loc]
+                   (Files/delete (zip/node loc)))))))
+
+(defn cp
+  [from to]
+  (Files/copy (->path from) (->path to) (into-array CopyOption [])))
+
+(defn create
+  "Creates directories in path"
+  [path dirs]
+  (doseq [dir dirs]
+    (mkdir path dir)))
+
+(defn files
+  "A sequence of paths within path which are regular files, path must
+   be a path to a directory"
+  [path]
+  (let [children (children path)]
+    (filter file? children)))
+
+(defn mass-files
+  "A sequence of paths within path which are regular files, recursive"
+  [path]
+  (azip/keep (fn [loc]
+               (let [node (zip/node loc)]
+                 (when (file? node)
+                   node)))
+             (path-zip (->path path))))
+
+(defn mv
+  [src dst & [options]]
+  (try (let [options-array
+             (if options
+               (if (:overwrite? options)
+                 [StandardCopyOption/REPLACE_EXISTING]
+                 [])
+               [])
+             copy-options (into-array CopyOption options-array)]
+         (Files/move (->path src) (->path dst) copy-options))
+       (catch Exception ex (println (.getMessage ex)))))
+
+(defn mass-rename
+  "Renames file-names that satisfy pred by transform, ancestor root"
+  [{:keys [root pred transform test? overwrite?]}]
+  (postwalk! (path-zip (->path root))
+             (fn [loc]
+               (let [node (zip/node loc)]
+                 (when (file? node)
+                   (let [file-name (str (last node))]
+                     (when (pred file-name)
+                       (let [new-file-name (transform file-name)
+                             directory (.getParent node)
+                             new-path (.resolve directory (->path new-file-name))]
+                         (if test?
+                           (println "mv: " node " -> " new-path)
+                           (mv node new-path {:overwrite? overwrite?}))))))))))
+
+(defn rm [file]
+  (let [path (->path file)]
+    (when (file? path)
+      (Files/delete path))))
+
+(defn rm-files [{:keys [root pred test?]}]
+  (doseq [file (files (->path root))]
+    (let [suffix (str (last file))]
+      (when (pred suffix)
+        (if test?
+          (println "rm: " suffix)
+          (rm file))))))
---
+++ src/fs2/watch.clj
@@ -0,0 +1,138 @@
+(ns fs2.watch
+  "A controller to monitor filesystem changes, with appropriate callback hooks"
+  (:require [extension.zip :refer [prewalk!]]
+            [clj-time.core :as t]
+            [clojure.zip :refer [node]]
+            [fs2.core :refer [directory? path-zip ->path files]])
+  (:import [java.nio.file Path WatchEvent WatchService FileSystems
+            StandardWatchEventKinds WatchKey Paths]
+           [org.apache.commons.vfs2.impl DefaultFileMonitor]
+           [org.apache.commons.vfs2 FileListener VFS]))
+
+(def event-keys
+  {StandardWatchEventKinds/ENTRY_CREATE :create
+   StandardWatchEventKinds/ENTRY_DELETE :delete
+   StandardWatchEventKinds/ENTRY_MODIFY :modify})
+
+(defn register
+  [path watcher]
+  (.register ^Path path
+             ^WatchService watcher
+             (into-array (keys event-keys))))
+
+(defn append-keys!
+  "Return a function which when given a path, recursively appends watchers"
+  [watcher watch-keys]
+  (fn [path]
+    (prewalk! (path-zip path)
+              (fn [loc] (let [path (node loc)]
+                          (when (directory? path)
+                            (let [key (register path watcher)]
+                              (swap! watch-keys conj [key path]))))))))
+
+(defn handler
+  [key watch-keys callback-map]
+  (let [path (@watch-keys key)
+        watch-events (.pollEvents key)]
+    (doseq [event watch-events]
+      (let [base-name (.context event)
+            path (.resolve path base-name)
+            action (event-keys (.kind event))]
+        (when-let [callback (callback-map action)]
+          (callback path))
+        (.reset key)))))
+
+(defn url->path [url]
+  (Paths/get (.toURI url)))
+
+(defn file-object->path [file-object]
+  (let [url (.getURL file-object)]
+    (url->path url)))
+
+(defn path->file-object [path]
+  (let [uri (.toUri path)
+        uri-string (str uri)
+        fs (VFS/getManager)]
+    (.resolveFile fs uri-string)))
+
+(defn poll-watch
+  "Callback functions for :create :delete :modify events on a path.
+   Each callback takes a java.nio.file.Path, called for side effects.
+   Return a function of no arguments, which closes the watcher"
+  [path callback-map]
+  (let [file-object (path->file-object path)
+        callback
+        (fn [callback-key]
+          (fn [event]
+            (let [path (file-object->path (.getFile event))]
+              (when-let [f (callback-key callback-map)]
+                (f path)))))
+        
+        monitor
+        (DefaultFileMonitor.
+          (reify FileListener
+            (fileChanged [this event]
+              ((callback :modify) event))
+            (fileCreated [this event]
+              ((callback :create) event))
+            (fileDeleted [this event]
+              ((callback :delete) event))))]
+    (.setRecursive monitor true)
+    (.addFile monitor file-object)
+    (.start monitor)
+    (fn [] (.stop monitor))))
+
+(defn watch
+  "Register callback functions for :create :delete :modify events on a path.
+   Each callback function has a single java.nio.file.Path argument, called for
+   side effects"
+  [path callback-map]
+  (let [watcher (.newWatchService (FileSystems/getDefault))
+        watch-keys (atom {})
+        watching? (atom true)
+        appender! (append-keys! watcher watch-keys)
+        callback-map (assoc callback-map :create (fn [path]
+                                                   (when (directory? path)
+                                                     (appender! path))))]
+
+    (appender! path)
+
+    (future
+      (while @watching?
+        (let [key ^WatchKey (.take watcher)]
+          (handler key watch-keys callback-map))))
+    
+    (fn []
+      (reset! watching? false)
+      (.close watcher))))
+
+(def path-list
+  (atom []))
+
+(defn seconds->millis
+  [s]
+  (-> s t/seconds .toStandardDuration .getMillis))
+
+(defonce watching?
+  (atom false))
+
+(defn schedule
+  "Calls function f every s seconds for side effects"
+  [f s]
+  (future
+    (while @watching?
+      (do (Thread/sleep (seconds->millis s))
+          (f)))))
+
+(defn force-watch
+  "Sometimes even polling is not enough, for each file directly in path, call
+   callback on the file every s seconds"
+  [path callback s]
+  (reset! watching? true)
+  (schedule (fn []
+              (doseq [path (files (->path path))]
+                (callback path)))
+            s))
+
+(defn stop-force-watch []
+  (reset! watching? false))
