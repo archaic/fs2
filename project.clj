b95bf332cb6ae21e6424717d5596f395b064e1b5
Christopher Rosengren <christopher.rosengren@gmail.com>
2015-02-14 14:10:44 +1100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.eoneq/fs2 "0.6.0"
+(defproject com.eoneq/fs2 "0.6.1"
   :description "File System Utilities"
-  :dependencies [[clj-time "0.8.0"]
-                 [org.clojure/clojure "1.6.0"]]
+  :dependencies [[clj-time "0.9.0"]
+                 [org.clojure/clojure "1.7.0-alpha5"]]
   :global-vars {*warn-on-reflection* true})
