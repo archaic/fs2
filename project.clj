855eb5d733fa45afeaab097298bd538c7dffe5f1
Christopher Rosengren <christopher.rosengren@gmail.com>
2015-08-22 15:54:15 +1000

    Remove time dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject com.eoneq/fs2 "0.6.3"
+(defproject com.eoneq/fs2 "0.6.4"
   :description "File System Utilities"
-  :dependencies [[clj-time "0.10.0"]
-                 [org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :global-vars {*warn-on-reflection* true})
